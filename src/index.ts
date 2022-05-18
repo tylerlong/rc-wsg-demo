0832cbd3bea08ea6a882c876cc8f98d8074d6bf8
Tyler Long <tyler4long@gmail.com>
2022-05-18 08:45:55 -0700

    Hello RingCentral

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import {SDK} from '@ringcentral/sdk';
+import WebSocketExtension from '@rc-ex/ws';
+
+const rcsdk = new SDK({
+  server: SDK.server.sandbox,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rcsdk.login({
+    username: process.env.RINGCENTRAL_USERNAME!,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD,
+  });
+  const r = await rcsdk.platform().get('/restapi/v1.0/account/~/extension/~');
+  console.log(JSON.stringify(await r.json(), null, 2));
+};
+
+main();
